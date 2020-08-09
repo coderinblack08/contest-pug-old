a9abd82a47d76a2ab576a9f5b9ae970140df97ba
coderinblack <kevinlu.email@gmail.com>
2020-08-09 12:36:33 -0700

    Add Feathers client

---
+++ client/api/feathers-client.ts
@@ -0,0 +1,10 @@
+import io from 'socket.io-client';
+import feathers from '@feathersjs/feathers';
+import socketio from '@feathersjs/socketio-client';
+
+const socket = io('http://localhost:3030');
+const client = feathers();
+
+client.configure(socketio(socket));
+
+export default client;
