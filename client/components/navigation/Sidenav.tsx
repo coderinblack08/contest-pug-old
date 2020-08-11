36385a323bae12d0f73377901417b61585006802
coderinblack <kevinlu.email@gmail.com>
2020-08-11 15:26:10 -0700

    :sparkles: Fix production error

--- client/components/navigation/Sidenav.tsx
+++ client/components/navigation/Sidenav.tsx
@@ -97,14 +97,20 @@ const Sidenav: React.FC = () => {
             <path d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
         </div>
-        <div className="flex flex-col ml-3">
-          <h2 className="text-gray-800 font-medium text-lg leading-none">
-            {user.name}
-          </h2>
-          <p className="text-gray-700 text-md">
-            @{user.name.toLowerCase().trim().split(' ').join('')}
-          </p>
-        </div>
+        {(() => {
+          if (user) {
+            return (
+              <div className="flex flex-col ml-3">
+                <h2 className="text-gray-800 font-medium text-lg leading-none">
+                  {user.name}
+                </h2>
+                <p className="text-gray-700 text-md">
+                  @{user.name.toLowerCase().trim().split(' ').join('')}
+                </p>
+              </div>
+            );
+          }
+        })()}
         <div className="ml-auto cursor-pointer">
           <svg
             fill="none"
