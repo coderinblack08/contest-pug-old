f90166c5f4a8e00a953f93bfcfa57b19efd88394
coderinblack <kevinlu.email@gmail.com>
2020-08-20 14:37:10 -0700

    :rocket: Fix for production

--- node_modules/vue-ctk-date-time-picker/nuxt/plugin.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue'
-import VueCtkDateTimePicker from 'vue-ctk-date-time-picker'
-import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css'
-
-Vue.component('VueCtkDateTimePicker', VueCtkDateTimePicker)
